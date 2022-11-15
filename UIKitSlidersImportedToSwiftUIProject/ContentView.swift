768a3c87b052ef75765ffc9fcd51f716aa7334e8
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-16 01:02:03 +0700

    Game logic

--- UIKitSlidersImportedToSwiftUIProject/ContentView.swift
+++ UIKitSlidersImportedToSwiftUIProject/ContentView.swift
@@ -10,11 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sliderValue = Float.random(in: 0 ... 100)
+    @State private var targetValue = Int.random(in: 0...100)
+    
     var body: some View {
         VStack {
-            SliderFromUIKit(sliderValue: $sliderValue)
+            Text("Подвиньте слайдер как можно ближе к \(Int(targetValue))")
+            SliderFromUIKit(sliderValue: $sliderValue, thumbAlpha: CGFloat(computeScore())/100)
+            Text("вот: \(CGFloat(computeScore())/100)")
+            Button(action: {}) {
+                Text("Проверь меня")
+            }
+            .padding()
+            Button(action: {}) {
+                Text("Начать заново")
+            }
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(lround(Double(sliderValue)) - targetValue)
+        return 100 - difference
     }
 }
 
--- UIKitSlidersImportedToSwiftUIProject/SliderFromUIKit.swift
+++ UIKitSlidersImportedToSwiftUIProject/SliderFromUIKit.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct SliderFromUIKit: UIViewRepresentable {
     @Binding var sliderValue: Float
+    let thumbAlpha: CGFloat
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
         slider.value = sliderValue
         slider.minimumValue = 0
         slider.maximumValue = 100
-        slider.thumbTintColor = .blue.withAlphaComponent(0.05)
+        
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
         uiView.value = sliderValue
+        uiView.thumbTintColor = .blue.withAlphaComponent(thumbAlpha)
     }
+    
 }
 
 struct SliderFromUIKit_Previews: PreviewProvider {
     static var previews: some View {
-        SliderFromUIKit(sliderValue: .constant(10))
+        SliderFromUIKit(sliderValue: .constant(10), thumbAlpha: 0.05)
     }
 }
