ec4a5d9e4287db006de7c9380c94ae13cd7d33f0
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-15 16:24:12 +0700

    Initial Commit

---
+++ UIKitSlidersImportedToSwiftUIProject/UIKitSlidersImportedToSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSlidersImportedToSwiftUIProjectApp.swift
+//  UIKitSlidersImportedToSwiftUIProject
+//
+//  Created by Olga Yurchuk on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSlidersImportedToSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
